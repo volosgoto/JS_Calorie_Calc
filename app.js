6cfade40e93d731b7fb49279932d3c7f0e40b5e7
volosgoto <volosgoto@yahoo.com>
2018-09-05 14:18:20 +0300

    add controllers

---
+++ app.js
@@ -0,0 +1,17 @@
+// Storage Controller
+let StorageController = (function () {
+})();
+
+// Item Controller
+let ItemController = (function () {
+})();
+
+
+// UI COntroller
+let UIController = (function () {
+})();
+
+// App Controller
+let AppController = (function () {
+})();
+
