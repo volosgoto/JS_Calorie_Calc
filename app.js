a2a71778b1f4ea42bf689eaba7441ce6cdc9b523
volosgoto <volosgoto@yahoo.com>
2018-09-05 15:42:38 +0300

    add AppContoller

--- app.js
+++ app.js
@@ -1,17 +1,62 @@
 // Storage Controller
-let StorageController = (function () {
-})();
+// let StorageController = (function () {
+// })();
+
 
 // Item Controller
+
+
 let ItemController = (function () {
+    // Item Construcror
+    function Item(id, name, calories) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+
+    // Data Structure
+    let data = {
+        items: [
+            { id: 0, name: 'Pop Corn', calories: 960 },
+            { id: 1, name: 'Stew', calories: 1200 },
+            { id: 2, name: 'Cerial', calories: 600 }
+        ],
+        currentItem: null,
+        totalCalories: 0
+    }
+
+    // Public methods
+    return {
+        logData: () => {
+            return data;
+        }
+    }
+    
 })();
 
 
 // UI COntroller
 let UIController = (function () {
+    
+    // Public methods
+    return {
+    
+    }
 })();
+
 
 // App Controller
-let AppController = (function () {
-})();
+let AppController = (function (ItemController, UIController) {
+    
 
+    // Public methods
+    return {
+        init: function () {
+            console.log('Init App...');
+        }
+    }
+})(ItemController, UIController);
+
+
+// Init App
+AppController.init();
